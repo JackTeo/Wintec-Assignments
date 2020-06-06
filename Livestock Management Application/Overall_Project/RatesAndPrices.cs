7c69e625b13c0324c4bb0e3667a5855d91ddf25b
Jack Teo <jinkiateo91@github.com>
2020-06-06 12:12:04 +1200

    initial commit

---
+++ Livestock Management Application/Overall_Project/RatesAndPrices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overall_Project
+{
+    class RatesAndPrices
+    {
+        public static double milkPriceCow; //Price of cow's milk per litre
+        public static double milkPriceGoat; //Price of goat's milk per litre
+        public static double woolPriceSheep; //Price of sheep's wool per kg
+        public static double waterPrice; //Price of water in cube
+        public static double tax; //Tax per kg
+        public static double jersyCowTax; //Tax of Jersy cow per kg
+
+    }
+}
---
+++ Livestock Management Application/Overall_Project/Sheep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overall_Project
+{
+    class Sheep : Livestock //subclass of Livestock
+    {
+        private double amountOfWool;
+
+        public Sheep(int ID, String type, double amountOfWater, double dailyCost, double weight, int age, string colour, double amountOfWool)
+            : base(ID, type, amountOfWater, dailyCost, weight, age, colour)
+        {
+            this.amountOfWool = amountOfWool;
+        }
+
+        public override double getAmountOfWool() { return this.amountOfWool; } //amount of wool getter
+
+        public override double getProfit() //profit getter
+        {
+            return (amountOfWool * RatesAndPrices.woolPriceSheep) - (getAmountOfWater() * RatesAndPrices.waterPrice) - getDailyCost();
+        }
+
+        public override double getGovTax() //government tax getter
+        {
+            return (amountOfWool * RatesAndPrices.tax * 30);
+        }
+    }
+}
